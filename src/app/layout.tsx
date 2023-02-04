c20c3cbce7f6d0f7c944c1ae58eb96d4a685a1f3
Jeon Eonseok <jseokeon@gmail.com>
2023-02-04 17:22:52 +0900

    [Refactor/Layout] styled0compoenets 관련 파일 추가 및 use client 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import './globals.css';
+import StyledComponentsRegistry from './lib/registry';
 import Header from '@/components/layout/header/Header';
 import { Providers } from './providers';
 import Footer from '@/components/layout/footer/Footer';
@@ -28,9 +29,11 @@ export default function RootLayout({
         <div id='backdrop-root'></div>
         <div id='overlay-root'></div>
         <Providers>
-          <Header />
-          <Box>{children}</Box>
-          <Footer />
+          <StyledComponentsRegistry>
+            <Header />
+            <Box>{children}</Box>
+            <Footer />
+          </StyledComponentsRegistry>
         </Providers>
       </body>
     </html>
