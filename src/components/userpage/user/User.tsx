463776187e56f70f99a9f79e8c6e794663c744f0
Jeon Eonseok <jseokeon@gmail.com>
2023-02-04 21:45:52 +0900

    [Design/Mypage] 프로필 사진 추가

--- src/components/userpage/user/User.tsx
+++ src/components/userpage/user/User.tsx
@@ -9,7 +9,7 @@ const UserBox = styled.div`
   /* width: 40%; */
   width: 50%;
   min-width: 183px;
-  max-width: 283px;
+  max-width: 243px;
   display: flex;
   flex-direction: column;
   justify-content: flex-end;
--- src/components/userpage/user/UserInfo.tsx
+++ src/components/userpage/user/UserInfo.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Image from 'next/image';
 import styled from 'styled-components';
 
 const UserInfoBox = styled.div`
@@ -9,14 +10,33 @@ const UserInfoBox = styled.div`
   align-items: center;
   border: 1px solid #b69eff;
   border-radius: 8px;
+  padding: 20px;
+
+  div {
+    font-size: 20px;
+    font-weight: bold;
+  }
+  address {
+    font-size: 12px;
+  }
+`;
+const ImageDeco = styled(Image)`
+  padding: 0 10px 10px 10px;
+  border-radius: 50%;
+  overflow: hidden;
 `;
 
 const UserInfo = () => {
   return (
     <UserInfoBox>
-      <div>사진</div>
-      <div>닉네임</div>
-      <address>주소</address>
+      <ImageDeco
+        src='/images/for-demo/kitty.jpg'
+        alt='kitty'
+        width={150}
+        height={150}
+      />
+      <div>키티키티키팈</div>
+      <address>경기도 고양시</address>
     </UserInfoBox>
   );
 };
