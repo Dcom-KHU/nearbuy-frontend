8058ebd8b3856237f445b7a6be26c50ea309e3e2
Cherrie <cherriekim925@gmail.com>
2023-02-06 22:35:23 +0900

    [Design/MainPage] 메인 페이지 레이아웃 설정

--- src/components/mainpage/MainPage.tsx
+++ src/components/mainpage/MainPage.tsx
@@ -1,45 +1,45 @@
+import styled from "styled-components";
+
+const MainPageBlock = styled.div`
+  display: flex;
+  width: 90%;
+  padding: 40px;
+  justify-content: center;
+  align-items: center;
+  background: lightblue;
+
+  #temp-pic {
+    background-color: pink;
+    width: 300px;
+    height: 450px;
+    margin: 20px;
+  }
+
+  #temp-par {
+    padding: 50px;
+    background-color: gold;
+    width: 500px;
+    height: auto;
+  }
+`;
+
 export default function MainPage() {
   return (
     <>
-      <h1>Hello World1</h1>
-      <h1>Hello World2</h1>
-      <h1>Hello World3</h1>
-      <h1>Hello World4</h1>
-      <h1>Hello World5</h1>
-      <h1>Hello World6</h1>
-      <h1>Hello World7</h1>
-      <h1>Hello World8</h1>
-      <h1>Hello World9</h1>
-      <h1>Hello World10</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
+      <MainPageBlock>
+        <div id="temp-pic">Temp Picture</div>
+        <div id="temp-par">
+          If you're looking for random paragraphs, you've come to the right
+          place. When a random word or a random sentence isn't quite enough, the
+          next logical step is to find a random paragraph. We created the Random
+          Paragraph Generator with you in mind. The process is quite simple.
+          Choose the number of random paragraphs you'd like to see and click the
+          button. Your chosen number of paragraphs will instantly appear. While
+          it may not be obvious to everyone, there are a number of reasons
+          creating random paragraphs can be useful. A few examples of how some
+          people use this generator are listed in the following paragraphs.
+        </div>
+      </MainPageBlock>
     </>
   );
 }
