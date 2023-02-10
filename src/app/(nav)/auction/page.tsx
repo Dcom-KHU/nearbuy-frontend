f6bc8b60bc2b9646ddef816d241ada6ae6ad0fb7
Cherrie <cherriekim925@gmail.com>
2023-02-11 01:51:23 +0900

    [Feat/~Pages] '교환, 나눔, 경매, 공구' 페이지 레이아웃 구현

--- src/app/(nav)/auction/page.tsx
+++ src/app/(nav)/auction/page.tsx
@@ -1,6 +1,28 @@
 // 경매 페이지
 // our-domain.com/auction
+"use client";
+
+import styled from "styled-components";
+import CardsContainer from "../board/CardsContainer";
+import PageSort from "../board/PageSort";
+
+const SalePageBlock = styled.div`
+  //background: lavender;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export default function Auction() {
-  return <h1>경매 페이지</h1>;
+  return (
+    <>
+      <h1>경매 페이지</h1>
+      <SalePageBlock>
+        <PageSort />
+        <CardsContainer />
+      </SalePageBlock>
+    </>
+  );
 }
--- src/app/(nav)/exchange/page.tsx
+++ src/app/(nav)/exchange/page.tsx
@@ -1,6 +1,28 @@
 // 교환 페이지
 // our-domain.com/exchange
+"use client";
+
+import styled from "styled-components";
+import CardsContainer from "../board/CardsContainer";
+import PageSort from "../board/PageSort";
+
+const SalePageBlock = styled.div`
+  //background: lavender;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export default function Exchange() {
-  return <h1>교환 페이지</h1>;
+  return (
+    <>
+      <h1>교환 페이지</h1>
+      <SalePageBlock>
+        <PageSort />
+        <CardsContainer />
+      </SalePageBlock>
+    </>
+  );
 }
