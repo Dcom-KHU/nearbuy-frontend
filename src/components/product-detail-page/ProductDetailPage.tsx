5b8285f6f171d164ceb16f9665033fb55445bcf9
Jeon Eonseok <jseokeon@gmail.com>
2023-02-17 14:51:10 +0900

    [Feat/PDP] 전반적인 틀

--- src/components/product-detail-page/ProductDetailPage.tsx
+++ src/components/product-detail-page/ProductDetailPage.tsx
@@ -1,3 +1,36 @@
 export default function ProductDetailPage() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <div>
+        <div>
+          <div>큰 사진</div>
+          <div>
+            <div>작은 사진1</div>
+            <div>작은 사진2</div>
+            <div>작은 사진3</div>
+          </div>
+          <div>채팅하기</div>
+        </div>
+        <div>
+          <div>
+            <div>시간, 채팅, 관심, 조회</div>
+            <div>
+              <div>이름</div>
+              <div>도구들</div>
+            </div>
+            <div>가격</div>
+          </div>
+          <div>
+            <div>유저정보</div>
+            <div>태그들</div>
+          </div>
+          <div>
+            <div>거래희망장소</div>
+            <div>지도</div>
+          </div>
+        </div>
+      </div>
+      <div>본문 내용</div>
+    </div>
+  );
 }
