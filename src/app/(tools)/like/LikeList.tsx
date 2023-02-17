74f08f9e86148818c2ff07cf49276a751d65bbcd
Cherrie <cherriekim925@gmail.com>
2023-02-17 16:27:51 +0900

    [Feat/LikePage] 찜 페이지 필터(pagesort) 추가

--- src/app/(tools)/like/LikeList.tsx
+++ src/app/(tools)/like/LikeList.tsx
@@ -3,6 +3,7 @@
 import styled from "styled-components";
 import ListItem from "@/components/list/ListItem";
 import ListNav from "@/components/list/ListNav";
+import PageSort from "@/app/(nav)/board/PageSort";
 
 const LikeListBlock = styled.div`
   width: 80%;
@@ -11,8 +12,12 @@ const LikeListBlock = styled.div`
   flex-direction: column;
 `;
 
-const ListItemBlock = styled.div`
+const GaroLine = styled.div`
   border-top: 1px solid rgba(0, 0, 0, 0.2);
+`;
+
+const ListItemBlock = styled.div`
+  //border-top: 1px solid rgba(0, 0, 0, 0.2);
   padding: 30px 0;
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
@@ -24,9 +29,11 @@ const LikeList = () => {
   return (
     <LikeListBlock>
       <ListNav />
+      <GaroLine />
+      <div className="self-end pt-5 mr-8 w-40">
+        <PageSort />
+      </div>
       <ListItemBlock>
-        <ListItem />
-        <ListItem />
         <ListItem />
         <ListItem />
         <ListItem />
