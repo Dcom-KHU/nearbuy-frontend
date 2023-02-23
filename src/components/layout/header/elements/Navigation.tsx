f7226d5825c4d087024b29510029f86fd67acd6e
Jeon Eonseok <jseokeon@gmail.com>
2023-02-24 00:16:42 +0900

    [Refactor/Nav] nav바 코드 간결화

--- src/components/layout/header/elements/Navigation.tsx
+++ src/components/layout/header/elements/Navigation.tsx
@@ -5,7 +5,7 @@ import { closeMenu } from '@/store/menuToggle/menuToggleSlice';
 import Link from 'next/link';
 import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
-import '../../../../app/globals.css';
+import { DetailPageState } from '@/store/detailPage/detailPageSlice';
 
 const Nav = styled.nav`
   width: 70%;
@@ -34,65 +34,37 @@ const NavList = styled.ul`
   }
 `;
 
+type NavItem = {
+  href: string;
+  label: string;
+  type?: keyof DetailPageState;
+};
+const NAV_ITEMS: NavItem[] = [
+  { href: '/board', label: '전체' },
+  { href: '/sale', label: '판매', type: 'sale' },
+  { href: '/exchange', label: '교환', type: 'exchange' },
+  { href: '/free', label: '나눔', type: 'free' },
+  { href: '/auction', label: '경매', type: 'auction' },
+  { href: '/group', label: '공구', type: 'group' },
+];
+
 // 상단 헤더 네비 (전체 ~ 공구)
-// FIXME: 밑에 handler들 각각 만들지 말고, 하나로 뭉치는 등, 더 깔끔하게 할 순 없을까?
 const Navigation = () => {
   const dispatch = useDispatch();
   // 작은 nav 일때, 햄버거 버튼 토글
   const menuToggleHandler = () => {
     dispatch(closeMenu());
   };
-  // 판매 페이지 상태
-  const isSaleHandler = () => {
-    dispatch(setIsType('sale'));
-  };
-  // 교환 페이지 상태
-  const isExchangeHandler = () => {
-    dispatch(setIsType('exchange'));
-  };
-  // 나눔 페이지 상태
-  const isFreeHandler = () => {
-    dispatch(setIsType('free'));
-  };
-  // 경매 페이지 상태
-  const isAuctionHandler = () => {
-    dispatch(setIsType('auction'));
-  };
-  // 공구 페이지 상태
-  const isGroupHandler = () => {
-    dispatch(setIsType('group'));
-  };
   return (
     <Nav>
       <NavList>
-        <li onClick={menuToggleHandler}>
-          <Link href='/board'>전체</Link>
-        </li>
-        <li onClick={menuToggleHandler}>
-          <Link href='/sale' onClick={isSaleHandler}>
-            판매
-          </Link>
-        </li>
-        <li onClick={menuToggleHandler}>
-          <Link href='/exchange' onClick={isExchangeHandler}>
-            교환
-          </Link>
-        </li>
-        <li onClick={menuToggleHandler}>
-          <Link href='/free' onClick={isFreeHandler}>
-            나눔
-          </Link>
-        </li>
-        <li onClick={menuToggleHandler}>
-          <Link href='/auction' onClick={isAuctionHandler}>
-            경매
-          </Link>
-        </li>
-        <li onClick={menuToggleHandler}>
-          <Link href='/group' onClick={isGroupHandler}>
-            공구
-          </Link>
-        </li>
+        {NAV_ITEMS.map(({ href, label, type }) => (
+          <li key={href} onClick={menuToggleHandler}>
+            <Link href={href} onClick={() => type && dispatch(setIsType(type))}>
+              {label}
+            </Link>
+          </li>
+        ))}
       </NavList>
     </Nav>
   );
--- src/store/detailPage/detailPageSlice.tsx
+++ src/store/detailPage/detailPageSlice.tsx
@@ -1,68 +1,6 @@
-// import { createSlice } from '@reduxjs/toolkit';
-
-// const initialState = {
-//   sale: false,
-//   exchange: false,
-//   free: false,
-//   auction: false,
-//   group: false,
-// };
-
-// // detail page 판매, 교환, 나눔 상태 관리
-// // FIXME: 밑에 reducers 좀 더 간결하게, 깔끔하게 할 순 없을까?
-// const detailPageSlice = createSlice({
-//   name: 'detailPage',
-//   initialState,
-//   reducers: {
-//     setIsType(state, action) {
-//       Object.keys(state).forEach((key) => {
-//         state[key] = false;
-//       });
-//       state[action.payload] = true;
-//     },
-//   },
-// });
-
-// export const detailPageActions = detailPageSlice.actions;
-// export default detailPageSlice;
-
-// import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-
-// interface DetailPageState {
-//   sale: boolean;
-//   exchange: boolean;
-//   free: boolean;
-//   auction: boolean;
-//   group: boolean;
-// }
-
-// const initialState: DetailPageState = {
-//   sale: false,
-//   exchange: false,
-//   free: false,
-//   auction: false,
-//   group: false,
-// };
-
-// const detailPageSlice = createSlice({
-//   name: 'detailPage',
-//   initialState,
-//   reducers: {
-//     setIsType(state, action: PayloadAction<keyof DetailPageState>) {
-//       Object.keys(state).forEach((key) => {
-//         state[key as keyof DetailPageState] = false;
-//       });
-//       state[action.payload] = true;
-//     },
-//   },
-// });
-
-// export const detailPageActions = detailPageSlice.actions;
-// export default detailPageSlice;
-
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-interface DetailPageState {
+export interface DetailPageState {
   sale: boolean;
   exchange: boolean;
   free: boolean;
