f2a73c3ad38585fa38b714d878ea30c7ab0b13e5
Jeon Eonseok <jseokeon@gmail.com>
2023-02-26 18:10:04 +0900

    [Fix/Nav] nav바가 화면 100% 차지 못하는 오류 해결

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ import { useDispatch } from 'react-redux';
 
 // main 부분 내용 잘림 방지, 위에 header 만큼 margin-top
 const Box = styled.div`
-  margin-top: 108px;
+  margin-top: 88px;
   // TODO: 햄버거 버튼 누를 시, main 내용이 그 만큼 아래로 내려가도록 하고 싶음. (후순위)
   display: flex;
   flex-direction: column;
--- src/components/layout/header/LargeNav.tsx
+++ src/components/layout/header/LargeNav.tsx
@@ -6,18 +6,23 @@ import Navigation from './elements/Navigation';
 import Tools from './elements/Tools';
 import Login from './elements/Login';
 
-const HeaderBox = styled.div`
-  // 큰 화면
-  // TODO: 전체 화면 시, 양 옆 margin 부분 background-color 적용 안 됨.
+const Box = styled.div`
+  width: 100%;
+  height: 88px;
   background-color: rgb(255, 255, 255);
   position: fixed;
   top: 0;
   left: 50%;
   transform: translate(-50%, 0);
+`;
+const HeaderBox = styled.div`
+  // 큰 화면
+  // TODO: 전체 화면 시, 양 옆 margin 부분 background-color 적용 안 됨.
+  margin: 0 auto;
   font-size: 18px;
   width: 100%;
   max-width: 1200px;
-  padding: 30px 20px;
+  padding: 20px;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -38,12 +43,14 @@ const HeaderBox = styled.div`
 // 상단 헤더 (큰 화면일 때)
 const LargeNav = () => {
   return (
-    <HeaderBox>
-      <Logo />
-      <Navigation />
-      <Tools />
-      <Login />
-    </HeaderBox>
+    <Box>
+      <HeaderBox>
+        <Logo />
+        <Navigation />
+        <Tools />
+        <Login />
+      </HeaderBox>
+    </Box>
   );
 };
 export default LargeNav;
