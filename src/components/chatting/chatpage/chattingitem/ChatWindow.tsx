9ecbb8524c770a147485c341e60cd3ad41061a41
Jeon Eonseok <jseokeon@gmail.com>
2023-03-01 17:05:23 +0900

    [Feat/Chat] 채팅 입력란에 기능 버튼들 추가

--- src/components/chatting/chatpage/chattingitem/ChatWindow.tsx
+++ src/components/chatting/chatpage/chattingitem/ChatWindow.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Image from 'next/image';
 import styled from 'styled-components';
 
 const ChatWindowBox = styled.div`
@@ -11,8 +12,6 @@ const ChatWindowBox = styled.div`
   width: 100%;
 `;
 const BorderBox = styled.div`
-  display: flex;
-  align-items: center;
   border: 1px solid black;
   border-radius: 8px;
   padding: 10px 9px;
@@ -22,13 +21,44 @@ const BorderBox = styled.div`
     background: none;
   }
 `;
+const FlexBox = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 10px;
+`;
 
 // 채팅 보내는 부분
 export default function ChatWindow() {
   return (
     <ChatWindowBox>
       <BorderBox>
-        <textarea placeholder='입력을 하든지 말든지' />
+        <FlexBox>
+          <button>
+            <Image
+              src='/images/chatwindow/emoji.svg'
+              alt='emoji'
+              width={24}
+              height={24}
+            />
+          </button>
+          <textarea placeholder='입력을 하든지 말든지' />
+          <button>
+            <Image
+              src='/images/chatwindow/picture.svg'
+              alt='emoji'
+              width={24}
+              height={24}
+            />
+          </button>
+          <button>
+            <Image
+              src='/images/chatwindow/send.svg'
+              alt='emoji'
+              width={24}
+              height={24}
+            />
+          </button>
+        </FlexBox>
       </BorderBox>
     </ChatWindowBox>
   );
