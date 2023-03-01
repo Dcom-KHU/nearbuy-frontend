0b488461712bc4c175d4a44231a19680415aa078
Jeon Eonseok <jseokeon@gmail.com>
2023-03-01 10:37:39 +0900

    [Design/Chat] 채팅 시간 디자인 변경

--- src/components/chatting/chatpage/chattingitem/Time.tsx
+++ src/components/chatting/chatpage/chattingitem/Time.tsx
@@ -1,4 +1,8 @@
 // 채팅 보낸 시간
 export default function Time() {
-  return <h1 className='text-center pt-3 pb-5'>2월 11, 2023 11:36 오후</h1>;
+  return (
+    <h1 className='text-gray-400 text-center pt-3 pb-5'>
+      2월 11, 2023 11:36 오후
+    </h1>
+  );
 }
