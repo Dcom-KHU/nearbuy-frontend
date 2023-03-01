7d1d06780e9a39b5b80b9aa1734cfa36c3dcf9fd
Jeon Eonseok <jseokeon@gmail.com>
2023-03-01 23:12:58 +0900

    [Fix/Chat] 토글버튼에 따른  채티방 크기 조절

--- src/components/chatting/chatpage/Chatting.tsx
+++ src/components/chatting/chatpage/Chatting.tsx
@@ -1,25 +1,31 @@
 import { RootState } from '@/store/store';
 import { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
-import styled from 'styled-components';
+import styled, { CSSProperties, StyledComponentProps } from 'styled-components';
 import Talk from './chattingitem/Talk';
 import Time from './chattingitem/Time';
 
-const ChattingBox = styled.div`
+interface ChattingBoxProps extends StyledComponentProps<'div', any, {}, never> {
+  toggle?: boolean;
+  style?: CSSProperties;
+}
+const ChattingBox = styled.div<ChattingBoxProps>`
   overflow: auto;
-  max-height: calc(100% - 268px);
+  max-height: ${(props) =>
+    props.toggle ? 'calc(100% - 268px)' : 'calc(100% - 218px)'};
   padding: 0 56px;
   padding-top: 20px;
 `;
 // 채팅 내용
 export default function Chatting() {
+  const toggle = useSelector((state: RootState) => state.chatToggle.toggle);
   // 새로고침 시 제일 최근 채팅 내용이 보이도록 스크롤 제일 아래로 가게함
   const chatBoxRef = useRef(null);
   useEffect(() => {
     chatBoxRef.current.scrollTop = chatBoxRef.current.scrollHeight;
   }, []);
   return (
-    <ChattingBox ref={chatBoxRef}>
+    <ChattingBox toggle={toggle} ref={chatBoxRef}>
       <Time />
       <Talk />
       <Talk />
--- src/store/chatToggle/chatToggleSlice.tsx
+++ src/store/chatToggle/chatToggleSlice.tsx
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  toggle: false,
+  toggle: true,
 };
 
 const chatToggleSlice = createSlice({
