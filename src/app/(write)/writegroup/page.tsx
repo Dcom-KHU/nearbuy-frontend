9ab27dff15144b810b5902df0d0907b13ebd82cc
Cherrie <cherriekim925@gmail.com>
2023-03-06 00:13:29 +0900

    [Feat/Write] 공구 글쓰기 페이지 틀만 잡음

--- src/app/(write)/writegroup/page.tsx
+++ src/app/(write)/writegroup/page.tsx
@@ -1,6 +1,29 @@
 // 공구 글쓰기 페이지
 // our-domain.com/writegroup
 
+"use client";
+
+import styled from "styled-components";
+import ProductImage from "@/components/write/writepage/ProductImage";
+import GroupWriteForm from "@/components/write/writepage/writegroup/GroupWriteForms";
+
+const WriteSellBlock = styled.div`
+  // background-color: aliceblue;
+
+  width: 75%;
+  padding: 10px;
+`;
+
 export default function WriteGroup() {
-  return <h1>공구 글쓰기 페이지</h1>;
+  return (
+    <>
+      <WriteSellBlock>
+        <div className="self-start pl-8 py-5 text-[20px]">
+          공구 글쓰기 페이지
+        </div>
+        <ProductImage />
+        <GroupWriteForm />
+      </WriteSellBlock>
+    </>
+  );
 }
