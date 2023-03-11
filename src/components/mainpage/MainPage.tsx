d9377b028f14cc3c533dff27f0cf112684b8a4ac
Cherrie <cherriekim925@gmail.com>
2023-03-12 03:10:34 +0900

    [Fix/MainPage] eslint(no-unescaped-entities) warning 해결, react-icons 라이브러리 설치.

--- src/components/mainpage/MainPage.tsx
+++ src/components/mainpage/MainPage.tsx
@@ -74,6 +74,7 @@ export default function MainPage() {
   return (
     <>
       <MainBox color="lightblue">
+        <WriteToggle />
         <MainPageBlock color="lightblue">
           <Image
             src="/images/for-demo/icecream.jpg"
@@ -82,32 +83,32 @@ export default function MainPage() {
             height={500}
           />
           <div>
-            If youu&apos;re looking for random paragraphs, you've come to the
-            right place. When a random word or a random sentence isn't quite
-            enough, the next logical step is to find a random paragraph. We
-            created the Random Paragraph Generator with you in mind. The process
-            is quite simple. Choose the number of random paragraphs you'd like
-            to see and click the button. Your chosen number of paragraphs will
-            instantly appear. While it may not be obvious to everyone, there are
-            a number of reasons creating random paragraphs can be useful. A few
-            examples of how some people use this generator are listed in the
-            following paragraphs.
+            If you&apos;re looking for random paragraphs, you&apos;ve come to
+            the right place. When a random word or a random sentence isn&apos;t
+            quite enough, the next logical step is to find a random paragraph.
+            We created the Random Paragraph Generator with you in mind. The
+            process is quite simple. Choose the number of random paragraphs
+            you&apos;d like to see and click the button. Your chosen number of
+            paragraphs will instantly appear. While it may not be obvious to
+            everyone, there are a number of reasons creating random paragraphs
+            can be useful. A few examples of how some people use this generator
+            are listed in the following paragraphs.
           </div>
         </MainPageBlock>
       </MainBox>
       <MainBox color="lightyellow">
         <MainPageBlock color="lightyellow">
           <div>
-            If you're looking for random paragraphs, you've come to the right
-            place. When a random word or a random sentence isn't quite enough,
-            the next logical step is to find a random paragraph. We created the
-            Random Paragraph Generator with you in mind. The process is quite
-            simple. Choose the number of random paragraphs you'd like to see and
-            click the button. Your chosen number of paragraphs will instantly
-            appear. While it may not be obvious to everyone, there are a number
-            of reasons creating random paragraphs can be useful. A few examples
-            of how some people use this generator are listed in the following
-            paragraphs.
+            If you&apos;re looking for random paragraphs, you&apos;ve come to
+            the right place. When a random word or a random sentence isn&apos;t
+            quite enough, the next logical step is to find a random paragraph.
+            We created the Random Paragraph Generator with you in mind. The
+            process is quite simple. Choose the number of random paragraphs
+            you&apos;d like to see and click the button. Your chosen number of
+            paragraphs will instantly appear. While it may not be obvious to
+            everyone, there are a number of reasons creating random paragraphs
+            can be useful. A few examples of how some people use this generator
+            are listed in the following paragraphs.
           </div>
           <Image
             src="/images/for-demo/icecream.jpg"
@@ -126,16 +127,16 @@ export default function MainPage() {
             height={500}
           />
           <div>
-            If you're looking for random paragraphs, you've come to the right
-            place. When a random word or a random sentence isn't quite enough,
-            the next logical step is to find a random paragraph. We created the
-            Random Paragraph Generator with you in mind. The process is quite
-            simple. Choose the number of random paragraphs you'd like to see and
-            click the button. Your chosen number of paragraphs will instantly
-            appear. While it may not be obvious to everyone, there are a number
-            of reasons creating random paragraphs can be useful. A few examples
-            of how some people use this generator are listed in the following
-            paragraphs.
+            If you&apos;re looking for random paragraphs, you&apos;ve come to
+            the right place. When a random word or a random sentence isn&apos;t
+            quite enough, the next logical step is to find a random paragraph.
+            We created the Random Paragraph Generator with you in mind. The
+            process is quite simple. Choose the number of random paragraphs
+            you&apos;d like to see and click the button. Your chosen number of
+            paragraphs will instantly appear. While it may not be obvious to
+            everyone, there are a number of reasons creating random paragraphs
+            can be useful. A few examples of how some people use this generator
+            are listed in the following paragraphs.
           </div>
         </MainPageBlock>
       </MainBox>
