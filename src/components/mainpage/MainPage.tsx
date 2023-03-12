8d42a17714377e4b00b14cecda5682f354bd31fb
Cherrie <cherriekim925@gmail.com>
2023-03-12 16:22:09 +0900

    [Comment/Axios] Axios 사용할 때 참고용 주석 추가. 추후 지울 예정.

--- src/components/mainpage/MainPage.tsx
+++ src/components/mainpage/MainPage.tsx
@@ -30,6 +30,9 @@ const MainPageBlock = styled.section`
 `;
 
 interface Itemp {
+  // 언니가 정의해둔 type들
+  // API 리스폰스로 오는 값들을 결과값을 받아오는 값에 대해서 type을 적어줌(?)
+  //  API call response 데이터 값의 형식을 정리해서 넣어줌.
   id: number;
   detail: string;
   image: string[];
@@ -42,34 +45,55 @@ interface Itemp {
   type: string;
   writer: string;
 }
+
 export default function MainPage() {
-  // // get api call 하는 방법
-  // const {
-  //   data: getData,
-  //   isLoading: getIsLoading,
-  //   error: getError,
-  // } = useGet<Itemp>({
-  //   url: "/api/post/sale",
-  //   params: { id: 1 },
-  // });
+  // get api call 하는 방법 예시
+  const {
+    // 이 세개는 일단 useHttp 내의 useGet에서 주니까 받아온겨~
+    data: getData,
+    isLoading: getIsLoading,
+    error: getError,
+    // useGet 안에 Itemp로 타입을 넣어줄 수 있음. 없어도 동작하긴 하지만 있으면 나중에 쓸 일 있을테니까 일단 넣어줌. 웬만하면 넣는거 추천!
+    // data의 형식을 여기에다 type으로 적어둔거임. API call response 데이터 값의 형식을 정리해서 넣어줌.
+  } = useGet<Itemp>({
+    // 판매 게시글 조회 API 기준 예시. API 명세서에 있는 url이 /api/post/sale임
+    // useGet엔 RequestConfigType 오브젝트를 넣어줌.
+    //판매 게시글 조회 할 땐 API 명세서 기준 url(필수)랑 request parameter 안에 id 넘겨줘야 함.
+    url: "/api/post/sale", // 필수
+    params: { id: 1 },
+    // body를 넣어야 하는 경우 (예: feat/post-sale) 하단처럼 넣어줌. body는 data임!
+    // data: {},
+  });
 
-  // useEffect(() => {
-  //   console.log(getData, getIsLoading, getError);
-  // }, [getData, getIsLoading, getError]);
+  // 예시) useEffect를 통해 console.log 찍어보자
+  // useGet 해서 가져와진 response 값을 찍는거. API 명세서의 response 예시 형태와 동일함
+  useEffect(() => {
+    console.log(getData, getIsLoading, getError);
+  }, [getData, getIsLoading, getError]);
 
-  // // post api call 하는 방법
-  // const {
-  //   data: postData,
-  //   isLoading: postIsLoading,
-  //   error: postError,
-  // } = usePost<{ accessToken: string }>({
-  //   url: "/api/user/login",
-  //   data: { id: 1, password: "hii" },
-  // });
+  // post api call 하는 방법 예시 - get api call과 생김새는 다르지만 쓰는 방식은 비슷하다!
+  // 여기서는 feat/user-login 일반 로그인을 예시로 하겠음
+  const {
+    // usePost가 리턴하는 세 가지 값. 우리는 보통 data만 씀
+    data: postData,
+    isLoading: postIsLoading,
+    error: postError,
+    // 여기도 마찬가지로 accessToken: string은 꼭 없어도 됨
+  } = usePost<{ accessToken: string }>({
+    url: "/api/user/login", // 필수
+    // API 명세서에 body 넣어달라는 요청 있으므로 명세서에서 요구한 body의 id와 password 값을 data 값으로 받아옴
+    data: { id: 1, password: "hii" },
+    // params: {} // 만약 파라미터 넣어달라는 요청 있으면 이렇게 넣어주면 됨
+  });
 
-  // useEffect(() => {
-  //   console.log(postData, postIsLoading, postError);
-  // }, [postData, postIsLoading, postError]);
+  useEffect(() => {
+    console.log(postData, postIsLoading, postError);
+  }, [postData, postIsLoading, postError]);
+
+  /*
+    fetch는 로그인 한다음에 헤더에 accesstoken 세팅 해준다음에 요청을 보낼 수 있는 것들밖에 없어서 아직은 다룰 일 없음. 
+    사용 방법은 비슷! 
+  */
 
   return (
     <>
@@ -91,7 +115,7 @@ export default function MainPage() {
             you&apos;d like to see and click the button. Your chosen number of
             paragraphs will instantly appear. While it may not be obvious to
             everyone, there are a number of reasons creating random paragraphs
-            can be useful. A few examples of how some people use this generator
+            can be useful. A fsew examples of how some people use this generator
             are listed in the following paragraphs.
           </div>
         </MainPageBlock>
--- src/utils/customAxios.ts
+++ src/utils/customAxios.ts
@@ -1,7 +1,17 @@
+/*  Utils 폴더 안에 base URL 설정.
+  나중에 로그인 관련한 것들 헤더에 설정
+  이 파일은 axios 관련 설정 하려고 만듦
+*/
+
 import axios, { AxiosInstance } from "axios";
 import secrets from "../../secrets.json";
 
+// 지금은 크게 알 필요 없지만 진호오빠가 배포한 서버 url 설정 해두려고 만들어둠
+// API 콜 할 때 customAxios 사용하게끔 해서 useHttp.ts에서 get, fetch, post, delete 만들어둠. useHttp 코드는 궁금하면 보고 필수는 아님
+// MainPage.tsx에 사용법 있음
 const customAxios: AxiosInstance = axios.create({
+  // serverIP 깃허브에 올리지 말기~ 노출되면 안되는 정보임! gitignore 되는 secrets 파일에 담아놓기.
+  // secrets 파일에서 baseURL 읽어오는중
   baseURL: secrets.serverIP,
   headers: {
     // "Content-Type": "application/x-www-form-urlencoded",
