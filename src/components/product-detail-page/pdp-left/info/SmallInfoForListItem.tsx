7857bebf94ac0ddb61107861fd15e9adb657c594
Cherrie <cherriekim925@gmail.com>
2023-03-19 17:07:25 +0900

    [Design/List] D-10 혹은 1/5 이런식으로 표시되던 박스 줄바꿈문제 해결

--- src/components/product-detail-page/pdp-left/info/SmallInfoForListItem.tsx
+++ src/components/product-detail-page/pdp-left/info/SmallInfoForListItem.tsx
@@ -15,7 +15,7 @@ const Box = styled.div<BoxProps>`
   background-color: ${(props) =>
     props.type === "group" ? "#90bcff" : "#ffa1a1"};
   padding: 0 10px;
-  width: 57px;
+  min-width: 57px;
   text-align: center;
 `;
 
@@ -25,6 +25,7 @@ export default function SmallInfoForListItem({
   totalPeople,
   deadline,
 }: BoxProps) {
+  /* 지금 auction 게시물 deadline 값이 invalid하고 totalPeople 옆에 들어갈 '현재 참여중인 인원'에 대한 값이 없음*/
   if (type === "auction") {
     return (
       <Box type={type} totalPeople={totalPeople} deadline={deadline}>
