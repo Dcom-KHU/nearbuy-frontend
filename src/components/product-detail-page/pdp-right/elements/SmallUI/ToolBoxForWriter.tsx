8e143e13895e99260268c7c6f728967eeb170681
Cherrie <cherriekim925@gmail.com>
2023-03-26 20:47:54 +0900

    [Feat/DetailPage] 삭제 모달 구현

--- src/components/product-detail-page/pdp-right/elements/SmallUI/ToolBoxForWriter.tsx
+++ src/components/product-detail-page/pdp-right/elements/SmallUI/ToolBoxForWriter.tsx
@@ -4,15 +4,38 @@
 import Link from "next/link";
 import styled from "styled-components";
 import { useDelete } from "@/hooks/useHttp";
+import { useState, useRef } from "react";
 import {
   AiOutlineShareAlt,
   AiOutlineEdit,
   AiOutlineDelete,
 } from "react-icons/ai";
+import customAxios from "@/utils/customAxios";
 
 const ToolBoxForWriterBox = styled.div`
   display: flex;
   gap: 10px;
+`;
+
+const ModalOverlayBox = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalContainerBox = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  padding: 20px;
 `;
 
 interface PostDelProps {
@@ -34,17 +57,32 @@ export default function ToolBoxForWriter({ id }: { id: number }) {
     });
   */
 
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <ToolBoxForWriterBox>
-      <button title="공유하기">
-        <AiOutlineShareAlt color="dimgray" size={24} />
-      </button>
-      <Link href="/board" /* 게시글 수정 페이지로 연결 */ title="수정하기">
-        <AiOutlineEdit href="localhost:3000/sale" color="dimgray" size={24} />
-      </Link>
-      <button title="삭제하기">
-        <AiOutlineDelete color="dimgray" size={24} />
-      </button>
-    </ToolBoxForWriterBox>
+    <>
+      <ToolBoxForWriterBox>
+        <button title="공유하기">
+          <AiOutlineShareAlt color="dimgray" size={24} />
+        </button>
+        <Link href="/board" /* 게시글 수정 페이지로 연결 */ title="수정하기">
+          <AiOutlineEdit href="localhost:3000/sale" color="dimgray" size={24} />
+        </Link>
+        <button title="삭제하기" onClick={() => setShowModal(true)}>
+          <AiOutlineDelete color="dimgray" size={24} />
+        </button>
+      </ToolBoxForWriterBox>
+      {showModal && (
+        <ModalOverlayBox onClick={() => setShowModal(false)}>
+          <ModalContainerBox onClick={(e) => e.stopPropagation()}>
+            <h2>게시글을 삭제하시겠습니까?</h2>
+            <div>
+              <button /*onClick={handleDelete}*/>삭제</button>
+              <button onClick={() => setShowModal(false)}>취소</button>
+            </div>
+          </ModalContainerBox>
+        </ModalOverlayBox>
+      )}
+    </>
   );
 }
