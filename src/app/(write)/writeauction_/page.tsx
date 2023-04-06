121931c6a1429caa3f063fd852220f4e81fdd6d8
Charmull <527wlals@gmail.com>
2023-04-06 22:38:57 +0900

    [Feat/Post] 경매 게시글 포스팅

--- src/app/(write)/writeauction_/page.tsx
+++ src/app/(write)/writeauction_/page.tsx
@@ -4,6 +4,7 @@ import PostImageList from "@/components/write/writepage/common/ImageList";
 import PostFormBlock from "@/components/write/writepage/common/PostFormBlock";
 import customAxios from "@/utils/customAxios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import styled from "styled-components";
 
@@ -20,29 +21,53 @@ const WriteAuction = () => {
   const router = useRouter();
   const { register, handleSubmit } = useForm();
 
+  // 이미지 파일
+  const [images, setImages] = useState<File[]>([]);
+
+  // 거래 희망 장소
+  const [locations, setLocations] = useState<string[]>([]);
+
   const submitHandler = async (d: any) => {
-    console.log(d);
+    // console.log(d);
 
     const registeredData = {
       title: d.title,
       detail: d.detail,
-      image: ["test.png"],
-      location: d.location,
+      location: locations[0],
       tag: d.tag.split(" "),
       startPrice: d.startPrice,
       increasePrice: d.increasePrice,
       deadline: new Date(d.date).getTime(),
     };
 
-    const result = await customAxios
-      .post("/api/post/auction", registeredData, {
-        headers: {
-          Authorization:
-            "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjEiLCJyb2xlIjoiVVNFUiIsImlhdCI6MTY3OTQxMzE0MywiZXhwIjoxNjc5NDEzNzQzfQ.gKrmIAQQkMTZ2hNdHKo6i-12zoqYc8wtpyvJjilCl1w",
-        },
-      })
-      .then(data => {
-        console.log(data);
+    await customAxios
+      .post("/api/post/auction", registeredData)
+      .then(async data => {
+        // 이미지 가공
+        const formData = new FormData();
+        if (images) {
+          for (let i = 0; i < images.length; i++) {
+            formData.append("image", images[i]);
+          }
+        }
+
+        // 이미지 업로드
+        await customAxios
+          .post("/api/image/post", formData, {
+            headers: {
+              "Content-Type": "multipart/form-data;",
+            },
+            params: {
+              id: data.data.postId,
+            },
+          })
+          .then(data => {
+            console.log(data);
+          })
+          .catch(err => {
+            console.log(err);
+          });
+
         router.replace("/board");
       })
       .catch(err => {
@@ -56,10 +81,19 @@ const WriteAuction = () => {
         <div className="self-start pl-8 py-5 text-[20px]">경매 포스팅</div>
 
         {/* 이미지 리스트 */}
-        <PostImageList register={register} />
+        <PostImageList
+          register={register}
+          images={images}
+          setImages={setImages}
+        />
 
         {/* form */}
-        <PostFormBlock register={register} type="auction" />
+        <PostFormBlock
+          register={register}
+          locations={locations}
+          setLocations={setLocations}
+          type="auction"
+        />
         <form
           className="m-auto"
           onSubmit={handleSubmit(submitHandler, async d => console.log(d))}
