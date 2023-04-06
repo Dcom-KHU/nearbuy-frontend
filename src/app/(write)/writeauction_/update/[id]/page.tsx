09095f716b0da2730669c32f05a813e3fef7bbf9
Charmull <527wlals@gmail.com>
2023-04-07 03:36:18 +0900

    [Feat/Post] 경매 게시글 수정

---
+++ src/app/(write)/writeauction_/update/[id]/page.tsx
@@ -0,0 +1,225 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+// 공구 글쓰기 페이지
+// our-domain.com/writegroupbuying (추후 변경 예정)
+
+"use client";
+
+import styled from "styled-components";
+import ProductImage from "@/components/write/writepage/ProductImage";
+import GroupWriteForm from "@/components/write/writepage/writegroup/GroupWriteForms";
+import { useForm } from "react-hook-form";
+import PostImageList from "@/components/write/writepage/common/ImageList";
+import PostFormBlock from "@/components/write/writepage/common/PostFormBlock";
+import customAxios from "@/utils/customAxios";
+import { usePathname, useRouter } from "next/navigation";
+import { useRouter as Router } from "next/router";
+import { useEffect, useState } from "react";
+import KakaoMap from "@/components/write/writepage/common/KakaoMap";
+import PostImageListUpdate from "@/components/write/writepage/common/update/ImageUpdate";
+import { Dictionary } from "@reduxjs/toolkit";
+
+const WriteSellBlock = styled.div`
+  // background-color: aliceblue;
+
+  width: 75%;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+`;
+
+interface auctionGetData {
+  id: number;
+  type: string;
+  title: string;
+  writer: string;
+  detail: string;
+  image: string[];
+  time: number;
+  location: string;
+  tag: string[];
+  startPrice: number;
+  increasePrice: number;
+  currentPrice: number;
+  deadline: number;
+}
+
+const padNumber = (num: number, len: number) => {
+  return String(num).padStart(len, "0");
+};
+const formatDate = (dayNum: number) => {
+  let date = new Date(dayNum);
+  let year: string = "" + date.getFullYear(); // 년도
+  let month: string = padNumber(date.getMonth() + 1, 2); // 월
+  let day: string = padNumber(date.getDate(), 2); // 날짜
+
+  return [year, month, day].join("-");
+};
+
+export default function Page({
+  params,
+  searchParams,
+}: {
+  params: {
+    id: string;
+  };
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const [formValues, setFormValues] = useState<auctionGetData>();
+  const router = useRouter();
+  const { register, handleSubmit, reset } = useForm({
+    defaultValues: {
+      title: "",
+      detail: "",
+      tag: "",
+      startPrice: 0,
+      increasePrice: 0,
+      date: "",
+    },
+  });
+
+  // 이미지 파일
+  const [initialImgs, setInitialImgs] = useState<string[]>([]); // 기존 존재하던 image url 리스트 (origin)
+  const [newImgs, setNewImgs] = useState<FileList | undefined>(undefined); // 새 image 리스트 (new)
+  const [newPrevImgs, setNewPrevImgs] = useState<string[]>([]); // 화면에 보여주기 위한 새 image prev url 리스트 (new)
+  const [deleteImgs, setDeleteImgs] = useState<number[]>([]); // 기존 올려져있던 image 중 삭제할 image index 리스트 (origin)
+  const [initialImgDict, setInitialImgDict] = useState<{
+    [url: string]: number;
+  }>({}); // deleteImgs에 인덱스를 넣기 위해 기존 Image를 url: idx 형태로 담는 딕셔너리
+  // 리스트로 사진과 인덱스를 저장하면, 사진을 지웠을 때 재렌더링이 되어 각 사진의 인덱스가 변하기 때문에 해당 딕셔너리 state가 필요
+
+  // 거래 희망 장소
+  const [locations, setLocations] = useState<string[]>([]);
+
+  const submitHandler = async (d: any) => {
+    const registeredData = {
+      title: d.title,
+      detail: d.detail,
+      location: locations[0],
+      tag: d.tag.split(" "),
+      startPrice: JSON.parse(d.startPrice),
+      increasePrice: JSON.parse(d.increasePrice),
+      deadline: new Date(d.date).getTime(),
+    };
+
+    await customAxios
+      .patch("/api/post/auction", registeredData, {
+        params: { id: params.id },
+      })
+      .then(async data => {
+        // 이미지 업로드
+        newImgs &&
+          (await customAxios
+            .post("/api/image/post", newImgs, {
+              headers: {
+                "Content-Type": "multipart/form-data;",
+              },
+              params: {
+                id: params.id,
+              },
+            })
+            .then(data => {
+              console.log(data);
+            })
+            .catch(err => {
+              console.log(err);
+            }));
+
+        // 이미지 삭제
+        console.log(deleteImgs);
+        deleteImgs &&
+          (await customAxios
+            .delete("/api/image/post", {
+              data: { index: deleteImgs },
+              params: {
+                id: params.id,
+              },
+            })
+            .then(data => {
+              console.log(data);
+            })
+            .catch(err => {
+              console.log(err);
+            }));
+
+        // router.replace("/board");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    (async () => {
+      const getData = await customAxios.get("/api/post/auction", {
+        params: { id: params.id },
+      });
+      setFormValues(getData.data);
+    })();
+  }, []);
+
+  useEffect(() => {
+    console.log(formValues);
+    if (formValues) {
+      reset({
+        title: formValues.title,
+        detail: formValues.detail,
+        tag: formValues.tag.join(" "),
+        startPrice: formValues.startPrice,
+        increasePrice: formValues.increasePrice,
+        date: formatDate(formValues.deadline),
+      });
+
+      setLocations([formValues.location]);
+      setInitialImgs(formValues.image);
+
+      let tempDict: { [url: string]: number } = {};
+      formValues.image.forEach((url, i) => {
+        tempDict[url] = i;
+      });
+      setInitialImgDict(tempDict);
+    }
+  }, [formValues]);
+
+  return (
+    <>
+      <WriteSellBlock>
+        <div className="self-start pl-8 py-5 text-[20px]">경매 포스팅 수정</div>
+
+        {/* 이미지 리스트 */}
+        <PostImageListUpdate
+          initialImgs={initialImgs}
+          setInitialImgs={setInitialImgs}
+          newImgs={newImgs}
+          setNewImgs={setNewImgs}
+          newPrevImgs={newPrevImgs}
+          setNewPrevImgs={setNewPrevImgs}
+          setDeleteImgs={setDeleteImgs}
+          deleteImgs={deleteImgs}
+          initialImgDict={initialImgDict}
+        />
+
+        {/* form */}
+        <PostFormBlock
+          register={register}
+          locations={locations}
+          setLocations={setLocations}
+          type="auction"
+        />
+        <form
+          className="m-auto"
+          onSubmit={handleSubmit(submitHandler, async d => console.log(d))}
+        >
+          <button
+            className="min-w-[100px] bg-[#b8ddfd] p-[13px] border-[1px] rounded-md text-gray-600 hover:bg-sky-300"
+            type="submit"
+          >
+            등록하기
+          </button>
+        </form>
+
+        {/* 지도 연습 */}
+        {/* <KakaoMap /> */}
+      </WriteSellBlock>
+    </>
+  );
+}
