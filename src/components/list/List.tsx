aac4a64202d072e0e0d41ebe12ce56d780c2c294
Charmull <527wlals@gmail.com>
2023-04-06 20:40:36 +0900

    Revert "[Feat/List] 게시글 없을 때, 게시글 없음 문장 추가"
    This reverts commit 826729c6dde5561d2149f2289944424f91b888c7.

--- src/components/list/List.tsx
+++ src/components/list/List.tsx
@@ -86,7 +86,6 @@ const List = ({ dataList }) => {
   if (myPageList) {
     postDatas = dataList;
   }
-  const emptyData = postDatas?.length === 0;
 
   useEffect(() => {
     // console.log(getData, getIsLoading, getError);
@@ -105,12 +104,10 @@ const List = ({ dataList }) => {
 
   return (
     <>
-      <ListItemBox emptyData={emptyData}>
+      <ListItemBox>
         {/* isBoard는 데이터 전체 표시하기 위해 둠. isBoard가 true라는건 전체 페이지 보고있는것. 
             true니까 map을 써서 더미데이터의 모든 리스트 가져옴 */}
-        {emptyData ? (
-          <div>게시글이 없습니다.</div>
-        ) : isBoard ? (
+        {isBoard ? (
           <>
             {postDatas?.map((post) => (
               <ListItem key={post.id} nowState={post.type} post={post} />
@@ -119,7 +116,7 @@ const List = ({ dataList }) => {
         ) : (
           /* 전체 페이지가 아니라 판매나 교환 등의 페이지일때 
             -> isBoard가 false가 되고 EachList가 화면에 표시됨 */
-          <EachList dataList={postDatas} />
+          <EachList dataList={dataList} />
         )}
       </ListItemBox>
     </>
