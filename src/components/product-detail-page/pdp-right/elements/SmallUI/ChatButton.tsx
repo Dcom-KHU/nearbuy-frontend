e94b89609ed999dd8082e006510b63feeaebc3df
Cherrie <cherriekim925@gmail.com>
2023-04-09 23:00:20 +0900

    [Feat/DetailPage] 공구/경매 게시글일 때 채팅 버튼 UI 수정

--- src/components/product-detail-page/pdp-right/elements/SmallUI/ChatButton.tsx
+++ src/components/product-detail-page/pdp-right/elements/SmallUI/ChatButton.tsx
@@ -3,6 +3,8 @@
 import Link from "next/link";
 import styled from "styled-components";
 import CheckIfWriter from "../CheckIfWriter";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store/store";
 
 const ActiveChatButtonBlock = styled(Link)`
   background-color: var(--background-color);
@@ -15,6 +17,9 @@ const ActiveChatButtonBlock = styled(Link)`
   &:hover {
     color: var(--accent-color);
     font-weight: 600;
+  }
+  &.createChat {
+    width: 140px;
   }
 `;
 
@@ -39,11 +44,21 @@ export default function ChatButton({
   id: number;
   ongoing: boolean;
 }) {
+  const activeType = useSelector((state: RootState) => state.activePage.active);
   const isWriter = CheckIfWriter({ id });
 
   if (isWriter === true) {
-    // 글작성자일 때
-    return <ActiveChatButtonBlock href="#">채팅 확인</ActiveChatButtonBlock>;
+    if (activeType === "auction" || activeType === "group") {
+      // 글작성자 본인이고, 공구나 경매 게시글일 때
+      return (
+        <ActiveChatButtonBlock href="#" className="createChat">
+          단체 채팅
+        </ActiveChatButtonBlock>
+      );
+    }
+    // 글작성자이고 공구나 경매 게시글일 때
+    else
+      return <ActiveChatButtonBlock href="#">채팅 확인</ActiveChatButtonBlock>;
   } else if (isWriter === false) {
     if (ongoing === true) {
       // 글작성자 아니고 로그인 돼있는데 거래 아직 진행중일 때
