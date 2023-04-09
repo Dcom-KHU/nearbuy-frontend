6a6304b884208a39d725d7e319a0c54e87f0ad85
Cherrie <cherriekim925@gmail.com>
2023-04-10 07:18:42 +0900

    [Refactor/DetailPage] 참여자 조회 관련 모달 스타일링 코드 분리

--- src/components/product-detail-page/pdp-right/elements/Participate/ParticipantsAuction.tsx
+++ src/components/product-detail-page/pdp-right/elements/Participate/ParticipantsAuction.tsx
@@ -4,77 +4,12 @@ import { useState } from "react";
 import axios from "axios";
 import { serverIP } from "@/../secrets.json";
 import GetToken from "@/utils/getToken";
-import styled from "styled-components";
-
-const ModalOverlayBox = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ModalContainerBox = styled.div`
-  background-color: white;
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  padding: 27px;
-  border-radius: 20px;
-  width: 400px;
-
-  .parti-buttons {
-    border: solid 2px lightgray;
-    border-radius: 8px;
-    margin-left: 10px;
-    padding: 8px;
-  }
-  .line {
-    width: 100%;
-    border: solid 1px lightgray;
-    margin-bottom: 10px;
-  }
-  ul {
-    border: solid 1px lightgray;
-    border-radius: 3px;
-    min-width: max-content;
-    max-height: 200px;
-    overflow-y: auto;
-    margin-top: 10px;
-    padding: 5px 10px;
-  }
-  li {
-    // 개별 참여자 박스
-    display: flex;
-    justify-content: space-between;
-    padding: 3px 7px;
-    div:nth-child(2) {
-      color: #464646;
-    }
-    div:nth-child(3) {
-      color: #999999;
-    }
-  }
-`;
-
-const ParticipateButton = styled.button`
-  background-color: var(--background-color);
-  border-radius: 8px;
-  width: 130px;
-  text-align: center;
-  padding: 10px;
-  margin-top: 20px;
-  margin-right: 11px;
-  &:hover {
-    color: var(--accent-color);
-    font-weight: 600;
-  }
-`;
+import {
+  ModalOverlayBox,
+  ModalContainerBox,
+  ParticipateButton,
+} from "./StyleModal";
+import { ImHammer2 } from "react-icons/im";
 
 interface ParticipantsData {
   user: {
--- src/components/product-detail-page/pdp-right/elements/Participate/ParticipantsGroup.tsx
+++ src/components/product-detail-page/pdp-right/elements/Participate/ParticipantsGroup.tsx
@@ -4,65 +4,11 @@ import { useState } from "react";
 import axios from "axios";
 import { serverIP } from "@/../secrets.json";
 import GetToken from "@/utils/getToken";
-import styled from "styled-components";
-
-const ModalOverlayBox = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ModalContainerBox = styled.div`
-  background-color: white;
-  position: fixed;
-  top: 50%;
-  left: 50%;
-
-  transform: translate(-50%, -50%);
-  padding: 27px;
-  border-radius: 20px;
-
-  .parti-buttons {
-    border: solid 2px lightgray;
-    border-radius: 8px;
-    margin-left: 10px;
-    padding: 8px;
-  }
-  .line {
-    width: 100%;
-    border: solid 1px lightgray;
-    margin-bottom: 3px;
-  }
-  li {
-    // 개별 참여자 박스
-    display: flex;
-    justify-content: space-between;
-    padding: 3px 7px;
-    div:nth-child(2) {
-      color: #999999;
-    }
-  }
-`;
-
-const ParticipateButton = styled.button`
-  background-color: var(--background-color);
-  border-radius: 8px;
-  width: 130px;
-  text-align: center;
-  padding: 10px;
-  margin-top: 20px;
-  margin-right: 11px;
-  &:hover {
-    color: var(--accent-color);
-    font-weight: 600;
-  }
-`;
+import {
+  ModalOverlayBox,
+  ModalContainerBox,
+  ParticipateButton,
+} from "./StyleModal";
 
 interface ParticipantsData {
   user: {
@@ -119,11 +65,10 @@ export default function ParticipantsGroup({
             <h2 style={{ paddingBottom: "5px", paddingLeft: "3px" }}>
               참여자 조회
             </h2>
-            <div className="line"></div>
             {isLoading ? (
               <div>Loading...</div>
             ) : (
-              <ul>
+              <ul className="group-participant">
                 <li>[주최자] {writer}</li>
                 {participants?.user?.map((participant) => (
                   <li key={participant.name}>
