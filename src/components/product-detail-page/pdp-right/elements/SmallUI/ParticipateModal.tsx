3b298b45f94246c50e45c7a6ff9d107e8f02ce04
Cherrie <cherriekim925@gmail.com>
2023-04-10 02:01:42 +0900

    [Feat/DetailPage] 공동구매 참여 관련 코드 분리

--- src/components/product-detail-page/pdp-right/elements/SmallUI/ParticipateModal.tsx
+++ src/components/product-detail-page/pdp-right/elements/SmallUI/ParticipateModal.tsx
@@ -6,36 +6,7 @@ import { serverIP } from "@/../secrets.json";
 import GetToken from "@/utils/getToken";
 import styled from "styled-components";
 import CheckIfWriter from "../CheckIfWriter";
-
-const ModalOverlayBox = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ModalContainerBox = styled.div`
-  background-color: white;
-  position: fixed;
-  top: 50%;
-  left: 50%;
-
-  transform: translate(-50%, -50%);
-  padding: 30px;
-  border-radius: 20px;
-
-  .parti-buttons {
-    border: solid 2px lightgray;
-    border-radius: 8px;
-    margin-left: 10px;
-    padding: 8px;
-  }
-`;
+import DoParticipate from "../DoParticipate";
 
 const ParticipateButton = styled.button`
   background-color: var(--background-color);
@@ -48,6 +19,9 @@ const ParticipateButton = styled.button`
   &:hover {
     color: var(--accent-color);
     font-weight: 600;
+  }
+  &.longer-button {
+    width: 130px;
   }
 `;
 
@@ -78,34 +52,16 @@ export default function ParticipateModal({ id }: { id: number }) {
 
   if (isWriter === false) {
     // 로그인 돼있고, 글작성자가 아닌 경우
+    return <DoParticipate id={id} />;
+  } else if (isWriter === true) {
+    // 글 작성자인 경우
     return (
-      <>
-        <ParticipateButton onClick={() => setParticipateModal(true)}>
-          참여하기
-        </ParticipateButton>
-        {ParticipateModal && (
-          <ModalOverlayBox onClick={() => setParticipateModal(false)}>
-            <ModalContainerBox onClick={(e) => e.stopPropagation()}>
-              <h2>공동구매에 참여하시겠습니까?</h2>
-              <div style={{ width: "250px", height: "60px" }}></div>
-              <div style={{ float: "right" }}>
-                <button className="parti-buttons" onClick={handleParticipate}>
-                  참여
-                </button>
-                <button
-                  className="parti-buttons"
-                  onClick={() => setParticipateModal(false)}
-                >
-                  취소
-                </button>
-              </div>
-            </ModalContainerBox>
-          </ModalOverlayBox>
-        )}
-      </>
+      <ParticipateButton className="longer-button">
+        참여자 확인
+      </ParticipateButton>
     );
   } else {
-    // 로그인 안돼있거나 글작성자인경우
+    // 로그인 안돼있는 경우
     return <></>;
   }
 }
