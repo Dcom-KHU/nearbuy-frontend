c4a0a44813c805b57037f78e584c15e737fdaa22
Cherrie <cherriekim925@gmail.com>
2023-04-09 17:03:34 +0900

    [Feat/DetaiPage] 판매자 데이터 불러오는 컴포넌트 분리

---
+++ src/components/product-detail-page/pdp-right/elements/ShowWriterInfo.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { serverIP } from "@/../secrets.json";
+import UserInfo from "./UserInfo";
+
+interface ShowWriterInfoProps {
+  writer: string | undefined;
+}
+
+// 상세페이지 내 판매자 데이터 불러오기
+export default function ShowWriterInfo({ writer }: ShowWriterInfoProps) {
+  const [userData, setUserData] = useState<any>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (writer) {
+          const response = await axios.get(`${serverIP}/api/user/page/name`, {
+            params: { name: writer },
+          });
+          setUserData(response.data);
+        }
+      } catch (error) {
+        console.log("error getting writer data");
+      }
+    };
+    fetchData();
+  }, [writer]);
+
+  if (!userData) {
+    return null;
+  }
+
+  return (
+    <>
+      <UserInfo infoData={userData} />
+    </>
+  );
+}
