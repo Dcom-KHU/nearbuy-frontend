2865c41ebd577dae1f6bed2f93465ea6e0321829
Cherrie <cherriekim925@gmail.com>
2023-04-21 00:50:29 +0900

    [Feat/Pagination] page 수 많아졌을 때 페이지 넘버 표시 블록 넘길 수 있도록 처리

--- src/components/list/List.tsx
+++ src/components/list/List.tsx
@@ -64,21 +64,13 @@ const PaginationButton = styled.button`
 // ************************************************************************************************
 // 해당 오류는 nowState 프로퍼티에 keyof DetailPageState 타입을 지정해두었지만, DUMMY_DATA 배열의 nowState 값은 string 형태이기 때문에 발생하는 오류입니다.
 // 해결 방법으로는 nowState 프로퍼티의 타입을 string으로 지정하거나, DUMMY_DATA의 nowState 값을 DetailPageState 타입의 enum 값으로 변경하는 방법이 있습니다.
-enum NowState {
-  Board = "board",
-  Sale = "sale",
-  Exchange = "exchange",
-  Free = "free",
-  Auction = "auction",
-  Group = "group",
-}
 
 interface Itemp {
   post: [
     {
       title: string;
       id: number;
-      image: string[];
+      image: string;
       location: string;
       type: string;
       salePrice: number | null;
@@ -96,6 +88,8 @@ interface Itemp {
 const List = ({ dataList }: { dataList?: any }) => {
   const [page, setPage] = useState(1);
   const [postDatas, setPostDatas] = useState<Itemp["post"]>();
+  const [startIndex, setStartIndex] = useState(1);
+  const [endIndex, setEndIndex] = useState(8);
 
   useEffect(() => {
     handlePageChange(1);
@@ -132,15 +126,15 @@ const List = ({ dataList }: { dataList?: any }) => {
   };
 
   const emptyData = postDatas?.length === 0;
-  console.log("dddd", emptyData);
 
   const isBoard = nowState === "board";
 
-  const totalPages = Math.ceil(85 / 12); // 페이지당 12개 게시글
+  const totalPages = Math.ceil(185 / 12); // 페이지당 12개 게시글
+  // FIXME : 추후 totalPages를  Math.ceil([전체 포스트 개수]/ 12); 로 바꿔야함
 
   const renderPageButtons = () => {
     const buttons = [];
-    for (let i = 1; i <= totalPages; i++) {
+    for (let i = startIndex; i <= endIndex; i++) {
       buttons.push(
         <PaginationButton
           key={i}
@@ -190,7 +184,27 @@ const List = ({ dataList }: { dataList?: any }) => {
           <EachList dataList={postDatas} />
         )}
       </ListItemBox>
-      <PaginationBox>{renderPageButtons()}</PaginationBox>
+      <PaginationBox>
+        <PaginationButton
+          disabled={startIndex === 1}
+          onClick={() => {
+            setStartIndex(startIndex - 8);
+            setEndIndex(endIndex - 8);
+          }}
+        >
+          {"<"}
+        </PaginationButton>
+        {renderPageButtons()}
+        <PaginationButton
+          disabled={endIndex >= totalPages}
+          onClick={() => {
+            setStartIndex(startIndex + 8);
+            setEndIndex(endIndex + 8);
+          }}
+        >
+          {">"}
+        </PaginationButton>
+      </PaginationBox>
     </>
   );
 };
