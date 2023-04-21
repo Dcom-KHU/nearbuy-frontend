604c0408e605deba5ca4fdbb6721bdee4cdc6313
Cherrie <cherriekim925@gmail.com>
2023-04-21 16:41:30 +0900

    [Feat/Pagination] 검색 결과에도 pagination 적용

--- src/components/search/SearchList.tsx
+++ src/components/search/SearchList.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import styled, { css } from "styled-components";
+import { useGet } from "@/hooks/useHttp";
 import SearchEachList from "./SearchEachList";
 import ListItem from "../list/ListItem";
-import { useGet } from "@/hooks/useHttp";
+import { serverIP } from "@/../secrets.json";
+import axios from "axios";
+import { PaginationBox, PaginationButton } from "../list/Pagination";
 
 const ListItemBox = styled.div<ListItemBoxProps>`
   width: 80%;
@@ -31,14 +35,6 @@ const ListItemBox = styled.div<ListItemBoxProps>`
 // ************************************************************************************************
 // 오류 있는데 그건 List.tsx 참고
 // ************************************************************************************************
-enum NowState {
-  Board = "board",
-  Sale = "sale",
-  Exchange = "exchange",
-  Free = "free",
-  Auction = "auction",
-  Group = "group",
-}
 
 interface ListItemBoxProps {
   emptyData?: boolean;
@@ -65,6 +61,32 @@ interface Itemp {
 }
 
 const SearchList = ({ searchKey }: { searchKey: string | null }) => {
+  const [page, setPage] = useState(1);
+  const [postDatas, setPostDatas] = useState<Itemp["post"]>();
+  const [totalPosts, setTotalPosts] = useState(0);
+  const [startIndex, setStartIndex] = useState(1);
+  const [endIndex, setEndIndex] = useState(8);
+
+  useEffect(() => {
+    handlePageChange(1);
+  }, []);
+
+  const handlePageChange = async (newPage: number) => {
+    setPage(newPage);
+    try {
+      const response = await axios.get(`${serverIP}/api/post/board/search`, {
+        params: { type: "all", page: newPage - 1, size: 12, search: searchKey },
+      });
+      // console.log("데이타:::", response.data.post);
+      setTotalPosts(response.data.total);
+      const newPostDatas = response.data.post;
+      setPostDatas(newPostDatas);
+    } catch (error) {
+      console.log("Error occurred while getting post/board datas", error);
+    }
+  };
+
+  /* 
   const {
     data: getData,
     // data를 useGet을 통해서 구조분해할당을 통해 받아옴. data를 getData라는 이름으로 받아옴.
@@ -72,23 +94,69 @@ const SearchList = ({ searchKey }: { searchKey: string | null }) => {
     error: getError,
   } = useGet<Itemp>({
     url: "/api/post/board/search",
-    params: { type: "all", size: 20, search: searchKey }, // 나머지 파라미터 일단 생략 (default값 있음)
-    // pagination 구현 안해두니까 size가 post 수보다 적으면 게시글 목록이 제대로 표시 안됨ㅠ
+    params: { type: "all", size: 20, search: searchKey }, 
   });
+  */
 
-  let postDatas = getData?.post || [];
+  // let postDatas = getData?.post || [];
 
   const emptyData = postDatas?.length === 0;
 
   // RootState는 타입스크립트 에러?땜시 추가했다 함
   // 설명과 에러는 List.tsx 파일 참고
 
-  //console.log("포스트데타: ", postDatas);
   const isBoard = true;
 
-  if (getError?.response?.status === 404) {
+  useEffect(() => {
+    if (page) {
+      const getDataAgain = async () => {
+        try {
+          const response = await axios.get(
+            `${serverIP}/api/post/board/search`,
+            {
+              params: {
+                type: "all",
+                page: page - 1,
+                size: 12,
+                search: searchKey,
+              },
+            }
+          );
+          setTotalPosts(response.data.total);
+          // console.log("데타:::", response.data.post);
+        } catch (error) {
+          console.log("Error occurred while getting post/board datas", error);
+        }
+      };
+      getDataAgain();
+    }
+  }, [page, searchKey]);
+
+  const totalPages = Math.ceil(totalPosts / 12); // 페이지당 12개 게시글
+  // FIXME : 추후 totalPages를  Math.ceil(totalPosts/ 12); 로 바꿔야함
+
+  const renderPageButtons = () => {
+    const buttons = [];
+    for (let i = startIndex; i <= endIndex; i++) {
+      buttons.push(
+        <PaginationButton
+          key={i}
+          onClick={() => handlePageChange(i)}
+          disabled={i === page || i > totalPages}
+          style={{ backgroundColor: i === page ? "lavender" : "transparent" }}
+        >
+          {i}
+        </PaginationButton>
+      );
+    }
+    return buttons;
+  };
+
+  /*
+  if (error?.response?.status === 404) {
     return <div>게시글이 없습니다.</div>;
   }
+*/
 
   return (
     <>
@@ -108,6 +176,27 @@ const SearchList = ({ searchKey }: { searchKey: string | null }) => {
           <SearchEachList dataList={postDatas} searchKey={searchKey} />
         )}
       </ListItemBox>
+      <PaginationBox>
+        <PaginationButton
+          disabled={startIndex === 1}
+          onClick={() => {
+            setStartIndex(startIndex - 8);
+            setEndIndex(endIndex - 8);
+          }}
+        >
+          {"<"}
+        </PaginationButton>
+        {renderPageButtons()}
+        <PaginationButton
+          disabled={endIndex >= totalPages}
+          onClick={() => {
+            setStartIndex(startIndex + 8);
+            setEndIndex(endIndex + 8);
+          }}
+        >
+          {">"}
+        </PaginationButton>
+      </PaginationBox>
     </>
   );
 };
